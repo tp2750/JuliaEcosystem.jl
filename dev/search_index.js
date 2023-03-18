c673b5deb8e49a9dc3bc5d332e99970b26042045
Documenter.jl <documenter@juliadocs.github.io>
2023-03-18 16:18:26 +0000

    build based on 24ae8c0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = JuliaEcosystem","category":"page"},{"location":"#JuliaEcosystem","page":"Home","title":"JuliaEcosystem","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for JuliaEcosystem.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [JuliaEcosystem]","category":"page"}]
+}
